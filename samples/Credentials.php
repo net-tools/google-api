23eafd3c6f9211cef3df1d2713f341d90bc2b4fa
Pierre <dev@net-tools.ovh>
2017-04-10 12:44:09 +0200

    credentials.php to be updated with user credentials

--- samples/Credentials.php
+++ samples/Credentials.php
@@ -7,13 +7,10 @@ ini_set('display_errors', 'stdout');
 // ============ TO BE UPDATED WITH YOUR GOOGLE DEV ACCOUNT DATA ==============
 
 // for CLIENT_ID and CLIENT_SECRET, refer to the values seen on the Google Developper console
-const CLIENT_ID = '622710147925-9v22ibp3rb3hnqrgh53jsr5aq33l0h1f.apps.googleusercontent.com';
-const CLIENT_SECRET = 'pZBelCqjMVCFbyjPFTaBtWMG';
+const CLIENT_ID = 'xxx.apps.googleusercontent.com';
+const CLIENT_SECRET = 'xxxx';
 
 // ============ /TO BE UPDATED WITH YOUR GOOGLE DEV ACCOUNT DATA ==============
-
-
-include $_SERVER['DOCUMENT_ROOT'] . '/libc-test/vendor/autoload.php';
 
 
 ?>
