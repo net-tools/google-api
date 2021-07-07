443749380e6c812610c866d9db50d110b19da3ed
Pierre <dev@nettools.ovh>
2021-07-07 17:58:06 +0200

    missing class

---
+++ src/Tools/PeopleSync/UnsupportedException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * UnsupportedException
+ *
+ * @author Pierre - dev@nettools.ovh
+ * @license MIT
+ */
+
+
+
+namespace Nettools\GoogleAPI\Tools\PeopleSync;
+
+
+
+
+/**
+ * Class for user exception
+ */
+class UnsupportedException extends UserException
+{
+}
+
+
+?>
