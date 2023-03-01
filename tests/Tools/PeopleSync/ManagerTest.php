f57289ce600f2c9da8a52fd9db9335a1a945a649
nettools.ovh <dev@nettools.ovh>
2023-03-01 12:13:39 +0100

    fix

--- tests/Tools/PeopleSync/ManagerTest.php
+++ tests/Tools/PeopleSync/ManagerTest.php
@@ -2075,7 +2075,7 @@ class SyncLog extends \Psr\Log\AbstractLogger
 	
 
     
-	public function log($level, $message, array $context = array())
+	public function log($level, Stringable|string $message, array $context = [])
 	{
 		$this->entries[] = (object)['level'=>$level, 'message'=>$message, 'context'=>$context];
 	}
